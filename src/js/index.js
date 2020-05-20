24e025ebd0e6b32dba1ff394fe1f1e29cbf53cff
Ufuoma Ogodo <ufuomaogodo@gmail.com>
2020-05-20 16:49:56 +0100

    added new close function for the resposive navbar added new fonts and changed some navbar styling

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,11 @@ const html = document.getElementById('html');
 const body = document.getElementById('body');
 const burger = document.getElementById('burger');
 const navlink = document.getElementById('navlink');
-
+const main = document.querySelector('main');
 window.addEventListener("scroll", () => {
     let y = html.scrollTop;
     if(y>20){
-        nav.style.borderBottom = "1px rgb(232, 232, 232) solid";
+        nav.style.borderBottom = "1px rgb(229, 229, 229) solid";
     }else{
         nav.style.borderBottom = "none";
     }
@@ -16,4 +16,9 @@ burger.addEventListener('click', () => {
    navlink.classList.toggle('navactive');
 })
 
+main.addEventListener('click', () =>{
+    if(navlink.classList.contains('navactive')){
+        navlink.classList.remove("navactive");
+    }
+})
 
